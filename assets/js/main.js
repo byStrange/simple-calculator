9f8135d94227c453a2bf34b465dc38bf42d0378a
Strange-bs <80538114+Strange-bs@users.noreply.github.com>
2021-12-10 16:49:46 +0500

    fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,13 @@ let bubbleParent = document.querySelector('.bubbles');
 let delays = [2.2, 3.5, 0.2, 6, 7, 4, 3];
 let scales = [2.15, 1.55, .8, 2.15, 3.4, 1];
 let buttons = [{
-    t: ['Ac', 7, 4, 1, 9]
+    t: ['Ac', 7, 4, 1, '0']
 }, {
     t: ['Del', 8, 5, 2, '.']
 }, {
-    t: ['%', 6, 3, '00']
+    t: ['%', 9, 6, 3, '00']
 }, {
-    t: ['/', '-', '+', '=']
+    t: ['/', '-', '+', '*', '=']
 }];
 let form = document.querySelector('form')
 for (let i = 0; i < 7; i++) {
@@ -19,7 +19,7 @@ for (let i = 0; i < 7; i++) {
     span.className = `sp-${i}`;
     bubbleParent.appendChild(span);
 }
-for (let c = 0; c < buttons.length; c++) {
+for (let c = 0; c < buttons.length + 1; c++) {
     let row = document.createElement('div');
     row.className = 'row'
     for (e of buttons) {
@@ -39,7 +39,9 @@ btns.forEach(they => {
         } else if (this.value == 'Del') {
             resultInput.value = resultInput.value.slice(0, resultInput.value.length - 1)
         } else if (this.value == '=') {
-            resultInput.value = eval(resultInput.value)
+            if (resultInput.value) {
+                resultInput.value = eval(resultInput.value)
+            }
         } else {
             resultInput.value += this.value
         }
